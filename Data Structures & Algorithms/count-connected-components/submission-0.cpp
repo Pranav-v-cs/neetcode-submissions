b6848fcd4a025a21ae79076b441d6f21c78d4c83
Pranav <pranav.vijayakumar216@gmail.com>
2026-06-28 22:07:10 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,44 @@
+class DSU {
+    public:
+        int n;
+        vector<int> parent;
+
+        DSU(int n) {
+            parent.resize(n);
+            for (int i = 0; i < n; i++)
+                parent[i] = i;
+        }
+
+        int find(int x) {
+            if (x == parent[x]) {
+                return x;
+            }
+            return find(parent[x]);
+        }
+
+        void unite(vector<int> v) {
+            int lx = find(v[0]);
+            int ly = find(v[1]);
+
+            if (lx != ly) {
+                parent[ly] = lx;
+            }
+        }
+};
+
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        DSU dsu(n);
+        unordered_set<int> s;
+
+        for (auto edge: edges) {
+            dsu.unite(edge);
+        }
+
+        for (auto a: dsu.parent) {
+            s.insert(dsu.find(a));
+        }
+        return s.size();
+    }
+};
